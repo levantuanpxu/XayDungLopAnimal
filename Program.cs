dc78f9d7596a04c922c74cf4637e761b2da8cd74
levantuanpxu <70925631+levantuanpxu@users.noreply.github.com>
2020-09-21 10:01:14 +0700

    XayDungLopAnimal
    XayDungLopAnimal

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text;
+
+namespace Xay_dung_lop_Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("20kg", "1.5", "kitty");
+            cat.PrintInfo();
+        }
+    }
+}
